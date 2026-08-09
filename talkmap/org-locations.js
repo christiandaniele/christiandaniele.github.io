59de94eeaf47777d6e38312407692cdd982f5f7a
Christian Daniele <christiandaniele@MacBook-Pro-di-Christian.local>
2026-08-09 10:27:06 +0200

    Update site layout and content

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Presentation at SIAM OP26<br />SIAM OP26; University of Edinburgh, Edinburgh, UK",
-    55.9440765,
-    -3.1883736
-  ],
-  [
-    "Poster on 'Mixed-Precision Newton's method' at M2L<br />M2L Summer School; University of Split, Split, Croatia",
-    43.5117478,
-    16.4676015
-  ],
-  [
-    "Poster on 'Mixed-Precision Newton's method' at SIAM Chapters Meeting<br />SIAM Chapters meeting for Young Researchers; PoliMi, Milan, Italy",
+    "Talk at SIAM Chapters Meeting Milano 2026<br />SIAM Chapters meeting for Young Researchers; PoliMi, Milan, Italy",
     45.4800101,
     9.2282387
   ],
   [
-    "Talk at PEQUAN Team meeting<br />PEQUAN Team meeting; Sorbonne University, Paris, France",
-    48.8468997,
-    2.3574487
+    "Presentation at SIAM OP26<br />SIAM OP26; University of Edinburgh, Edinburgh, UK",
+    55.9440765,
+    -3.1883736
   ]
 ];
